30116f65a084d251b50726fa9561ad8c04091351
Scott Newman <scott.newman@accenturefederal.com>
2023-10-04 10:28:24 -0400

    Added modals and improved page layout

--- post-app/src/app/app.component.ts
+++ post-app/src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from './post';
+import { PostService } from './post.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'post-app';
+export class AppComponent implements OnInit{
+  public posts: Post[];
+
+  constructor(private postService: PostService) {
+
+  }
+
+  ngOnInit(): void{
+      this.getPosts();
+  }
+
+  public splitArr(arr: Post[]) : Post[][] {
+    let newArr: Post[][] = [];
+    for(let i = 0; i< arr.length; i += 3) {
+      newArr.push(arr.slice(i, i+3));
+    }
+    return newArr;
+  }
+  
+  public getPosts(): void {
+    this.postService.getPosts().subscribe(
+      (response: Post[]) => {
+        this.posts = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onOpenModal(post: Post, mode: string) : void {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', 'addPostModal');
+    }
+    if (mode === 'add') {
+      button.setAttribute('data-target', 'addPostModal');
+    }
+    if (mode === 'add') {
+      button.setAttribute('data-target', 'addPostModal');
+    }
+  }
 }
---
+++ post-app/src/app/post.service.ts
@@ -0,0 +1,33 @@
+import { Observable } from 'rxjs';
+import { Post } from './post';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class PostService {
+    private apiServerUrl = environment.apiBaseUrl;
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    public getPosts(): Observable<Post[]> {
+        return this.http.get<Post[]>(`${this.apiServerUrl}/post/all`);
+    }
+
+    public addPost(post: Post): Observable<Post> {
+        return this.http.post<Post>(`${this.apiServerUrl}/post/add`, post);
+    }
+
+    public updatePost(post: Post): Observable<Post> {
+        return this.http.put<Post>(`${this.apiServerUrl}/post/update`, post);
+    }
+
+    public deletePost(postId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/post/delete/${postId}`);
+    }
+}
